9794c3c9d9315e589093b0440424a9a9f372dfcf
pks <pritam8956sonawane@gmail.com>
2020-05-23 23:26:35 +0530

    code added

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./header";
+import MemeGenerator from "./memeGenerator";
+
+
+function App() {
+    return (
+        <div>
+          
+          <Header/>
+          <MemeGenerator/>
+        </div>
+    )
+}
+
+export default App
